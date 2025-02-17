4ea2738ca54af1e0433706c3112907581010493b
sernili <li.sachse@gmail.com>
2025-02-17 15:48:25 +0100

    recipe list card - add data

--- src/app/recipelist/page.tsx
+++ src/app/recipelist/page.tsx
@@ -1,11 +1,74 @@
-import TagList from "@components/tags"; // TODO: find way to generalize import
-import RecipeListCard from "../../components/recipeListCard";
+import TagList from "@components/tags";
+import RecipeListCard from "@components/recipeListCard";
+
+export type Recipe = {
+  title: string;
+  key: string;
+  description?: string;
+  tags?: string[];
+  cookingTime?: { unit: string; number: number };
+  people?: number;
+  imageURL?: string;
+};
 
 export default function Page() {
+  const recipies: Recipe[] = [
+    {
+      title: "Spaghetti Carbonara",
+      key: "spaghetti_carbonara",
+      description: "Klassische italienische Pasta mit Ei und Speck.",
+      tags: ["Fleisch", "Salat"],
+      cookingTime: { unit: "Minuten", number: 25 },
+      people: 4,
+      imageURL: "https://picsum.photos/500/400",
+    },
+    {
+      title: "Avocado-Toast",
+      key: "avocado_toast",
+      description: "Gesundes Frühstück mit Avocado und Ei.",
+      tags: ["Frühstück"],
+      cookingTime: { unit: "Minuten", number: 10 },
+      people: 2,
+      imageURL: "https://picsum.photos/600/200",
+    },
+    {
+      title: "Griechischer Salat",
+      key: "griechischer_salat",
+      description: "Frischer Salat mit Feta, Gurke und Oliven.",
+      tags: ["Salat", "Vegetarisch"],
+      cookingTime: { unit: "Minuten", number: 15 },
+      people: 3,
+      imageURL: "https://picsum.photos/500/400",
+    },
+    {
+      title: "Vegetarische Lasagne",
+      key: "vegetarische_lasagne",
+      description: "Herzhafte Lasagne mit Gemüse und Käse.",
+      tags: ["Vegetarisch"],
+      cookingTime: { unit: "Minuten", number: 50 },
+      people: 6,
+      imageURL: "https://picsum.photos/500/500",
+    },
+    {
+      title: "Hähnchen-Curry",
+      key: "haehnchen_curry",
+      description: "Würziges Curry mit Hähnchen und Kokosmilch.",
+      tags: ["Fleisch", "Indisch"],
+      cookingTime: { unit: "Minuten", number: 40 },
+      people: 4,
+      imageURL: "https://picsum.photos/500/400",
+    },
+  ];
+
   return (
-    <div className="bg-secondary min-h-screen space-y-4 overflow-hidden py-6">
+    <div className="bg-secondary min-h-screen space-y-12 overflow-hidden py-6">
       <TagList />
-      <RecipeListCard />
+
+      <div className="space-y-6">
+        {recipies.map((recipe) => {
+          return <RecipeListCard recipe={recipe} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/recipeListCard.tsx
+++ src/components/recipeListCard.tsx
@@ -1,23 +1,45 @@
+import Recipe from "@app/recipelist/page";
 import Image from "next/image";
 
-export default function RecipeListCard() {
+export default function RecipeListCard({ recipe }: Recipe) {
   return (
     <div className="flex items-center justify-start gap-10 rounded-2xl bg-white shadow-lg">
       <Image
-        src="https://picsum.photos/500/400"
+        src={recipe.imageURL}
         alt="recipephoto"
-        className="h-[10rem] w-auto rounded-l-2xl"
+        className="h-[10rem] max-w-[12rem] overflow-hidden rounded-l-2xl object-cover"
         width={300}
         height={300}
       />
       <div className="flex flex-col gap-6 p-4">
         <div className="space-y-1">
-          <h2 className="text-primary font-serif text-4xl">Recipe Name</h2>
-          <p className="text-primary">Description</p>
+          <h2 className="text-primary font-serif text-4xl">{recipe.title}</h2>
+          <p className="text-primary">{recipe.description}</p>
         </div>
-        <div className="text-tertiary flex items-center gap-4">
-          <p>Tags</p> | <p>Zeit</p> | <p>Pers onen</p>
-        </div>
+
+        <p className="text-tertiary flex gap-2">
+          {recipe.tags && (
+            <span>
+              {recipe.tags.map((tag: [], index: number, array: []) => {
+                return `${tag}${
+                  index < array.length && index != array.length - 1 ? ", " : ""
+                }`;
+              })}
+            </span>
+          )}
+
+          <span>|</span>
+
+          {recipe.cookingTime && (
+            <span>
+              {recipe.cookingTime.number + " " + recipe.cookingTime.unit}
+            </span>
+          )}
+
+          <span>|</span>
+
+          {recipe.people && <span>{recipe.people + " Personen"}</span>}
+        </p>
       </div>
     </div>
   );
