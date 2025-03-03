9bb163d324be14e5c4cf7fbea3e2ad78ccdddeb1
Lisa Sachse <li.sachse@gmail.com>
2025-03-03 21:02:55 +0100

    remove github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  distDir: "./out",
-  reactStrictMode: false,
+  distDir: "build",
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
     domains: ["images.unsplash.com"],
-  },
-  assetPrefix: isProd ? "/2025-02-Yummy/" : "",
-  basePath: isProd ? "/2025-02-Yummy" : "",
-  webpack(config, options) {
-    config.resolve.extensions.push(".ts", ".tsx", ".jsx", ".js");
-    return config;
   },
 };
 
