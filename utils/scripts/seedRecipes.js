066987afe645d41ea4a36da2fa5b9361d88da1bf
Lisa Sachse <li.sachse@gmail.com>
2025-05-06 16:53:46 +0200

    add script to remove duplicates from database

--- utils/scripts/seedRecipes.js
+++ utils/scripts/seedRecipes.js
@@ -1,35 +1,6 @@
-import admin from "firebase-admin";
-import dotenv from "dotenv";
+import { db } from "./firebaseAdmin.js";
 import fs from "fs/promises";
 import path from "path";
-
-dotenv.config();
-
-const serviceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-
-if (!serviceAccountKey) {
-  console.error(
-    `Error: The FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set. - ${process.env}`,
-  );
-  process.exit(1);
-}
-
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(serviceAccountKey);
-} catch (error) {
-  console.error(
-    "Error parsing FIREBASE_SERVICE_ACCOUNT_KEY:" + process.env.title,
-    error,
-  );
-  process.exit(1);
-}
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
 
 async function seedDatabase() {
   try {
